9d2319ee00303022ccabdc4b876cb4c24c2ec536
Mariana Campos <69722854+marianac-campos@users.noreply.github.com>
2021-06-21 16:40:25 -0300

    Add files via upload

--- Bloco2/Banco de Dados Relacional/Exercicio04 - Cidade das Carnes.sql
+++ Bloco2/Banco de Dados Relacional/Exercicio04 - Cidade das Carnes.sql
@@ -2,7 +2,7 @@ create database db_cidade_das_carnes;
 use db_cidade_das_carnes;
 
 create table tb_categoria(
-	id bigint(5) auto_increment,
+	id int auto_increment,
 	descricao varchar(255) not null,
 	ativo boolean not null,
 	PRIMARY KEY(id)
@@ -11,37 +11,37 @@ create table tb_categoria(
 insert into tb_categoria (descricao, ativo) values ("Bovino", true);
 insert into tb_categoria (descricao, ativo) values ("Suino", true);
 insert into tb_categoria (descricao, ativo) values ("Aves", true);
-insert into tb_categoria (descricao, ativo) values ("Para Feijoada", true);
-insert into tb_categoria (descricao, ativo) values ("imbutidos", true);
-insert into tb_categoria (descricao, ativo) values ("soja", true);
-insert into tb_categoria (descricao, ativo) values ("outros", true);
+insert into tb_categoria (descricao, ativo) values ("Imbutidos", true);
+insert into tb_categoria (descricao, ativo) values ("Outros", true);
 
 select*from tb_categoria;
 
 create table tb_produtos (
-id bigint(5) auto_increment,
-nome varchar (255) not null,
-preco decimal(6,2) not null,
-qtproduto int not null,
-dtvalidade date,
-categoria_id bigint,
-PRIMARY KEY(id),
-FOREIGN KEY(categoria_id) references tb_categoria (id)
+	id int auto_increment,
+	nome varchar (255) not null,
+	preco decimal(6,2) not null,
+	qtproduto int not null,
+	categoria_id int,
+	FOREIGN KEY(categoria_id) references tb_categoria(id),
+    PRIMARY KEY(id)
 );
 
-insert into tb_produtos (nome, preco, qtProduto, dtvalidade, categoria_id) values ("ASA",40.00,30, "2021-11-07", 3);
-insert into tb_produtos (nome, preco, qtProduto, dtvalidade, categoria_id) values ("Picanha",20.00,30, "2021-11-08", 1);
-insert into tb_produtos (nome, preco, qtProduto, dtvalidade, categoria_id) values ("coxa de frango",20.00,30, "2021-11-07", 3);
-insert into tb_produtos (nome, preco, qtProduto, dtvalidade, categoria_id) values ("Bacon",30.00,30, "2021-11-09", 1);
-insert into tb_produtos (nome, preco, qtProduto, dtvalidade, categoria_id) values ("hamburguer",60.00,30, "2021-09-07", 5);
-insert into tb_produtos (nome, preco, qtProduto, dtvalidade, categoria_id) values ("Cupim",20.00,30, "2021-11-10", 1);
-insert into tb_produtos (nome, preco, qtProduto, dtvalidade, categoria_id) values ("Peito de frango",25.00,30, "2021-11-09", 3);
-insert into tb_produtos (nome, preco, qtProduto, dtvalidade, categoria_id) values ("Orelha de porco",20.00,30, "2021-11-07", 4);
-insert into tb_produtos (nome, preco, qtProduto, dtvalidade, categoria_id) values ("Salame",18.00,30, "2021-11-21", 5);
-insert into tb_produtos (nome, preco, qtProduto, dtvalidade, categoria_id) values ("medalhao",50.00,30, "2021-11-15", 3);
-insert into tb_produtos (nome, preco, qtProduto, dtvalidade, categoria_id) values ("mocoto",20.00,30, "2021-10-05", 2);
-insert into tb_produtos (nome, preco, qtProduto, dtvalidade, categoria_id) values ("chuleta",20.00,30, "2021-10-07", 1);
-insert into tb_produtos (nome, preco, qtProduto) values ("acendedor de churrasqueira",20.00,30);
-insert into tb_produtos (nome, preco, qtProduto) values ("soprador de churrasqueira",35.00,30);
+insert into tb_produtos (nome, preco, qtProduto, categoria_id) values ("Picanha",20.00, 30, 1);
+insert into tb_produtos (nome, preco, qtProduto, categoria_id) values ("Coxa de frango", 20.00, 30, 3);
+insert into tb_produtos (nome, preco, qtProduto, categoria_id) values ("Bacon", 30.00, 30, 2);
+insert into tb_produtos (nome, preco, qtProduto, categoria_id) values ("Hamburguer", 60.00, 30, 4);
+insert into tb_produtos (nome, preco, qtProduto, categoria_id) values ("Peito de frango", 25.00, 30, 3);
+insert into tb_produtos (nome, preco, qtProduto, categoria_id) values ("Salame", 18.00, 30, 4);
+insert into tb_produtos (nome, preco, qtProduto, categoria_id) values ("Avental", 20.00, 12, 5);
+insert into tb_produtos (nome, preco, qtProduto, categoria_id) values ("Soprador de churrasqueira", 35.00, 5, 5);
 
-select * from tb_produtos;
+select*from tb_produtos;
+
+select*from tb_produtos where preco > 30;
+select*from tb_produtos where preco between 14 and 27;
+select*from tb_produtos where nome like "%C%";
+
+select tb_produtos.nome, tb_produtos.preco, tb_categoria.descricao, tb_categoria.ativo
+from tb_produtos inner join tb_categoria on tb_categoria.id = tb_produtos.categoria_id;
+
+select*from tb_produtos where categoria_id in (1, 2);
